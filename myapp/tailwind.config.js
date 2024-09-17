1538cb459c4be45178311a68d44e316a90e0442c
wachirawut67 <wachirawut.pr.67@ubu.ac.th>
2024-09-17 23:31:42 +0700

    first commit

---
+++ myapp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Chakra Petch':['Chakra Petch']
+
+      }
+    },
+  },
+  plugins: [],
+}
+
